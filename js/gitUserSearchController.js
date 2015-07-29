5aa45f90cb83ea72d6b98568186f61e648ab05bd
Elin <elinnet@gmail.com>
2015-07-29 17:17:13 +0100

    creation of factory.  all tests pass

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,6 +1,6 @@
 githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
   var self = this;
-  var searchResource = $resource('https://api.github.com/search/users?key=#');
+  var searchResource = $resource('https://api.github.com/search/users?key=d423d0901c401fb4c4f5d2de4016c4e395741b28');
 
   self.doSearch = function() {
 
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -1,50 +1,57 @@
-beforeEach(module('GitUserSearch'));
-
-var ctrl;
-
-beforeEach(inject(function($controller) {
-  ctrl = $controller('GitUserSearchController');
-}));
-
 describe('GitUserSearchController', function() {
+
+  beforeEach(module('GitUserSearch'));
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('GitUserSearchController');
+  }));
 
   it('initialises with an empty search result and term', function() {
     expect(ctrl.searchResult).toBeUndefined();
     expect(ctrl.searchTerm).toBeUndefined();
   });
 
-});
+  describe('when searching for a user', function() {
 
-describe('when searching for a user', function() {
 
-  var items =  [
-      {
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      },
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }
-    ];
+    var items =  [
+        {
+          "login": "tansaku",
+          "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+          "html_url": "https://github.com/tansaku"
+        },
+        {
+          "login": "stephenlloyd",
+          "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+          "html_url": "https://github.com/stephenlloyd"
+        }
+      ];
 
-  var httpBackend;
-  beforeEach(inject(function($httpBackend) {
-    httpBackend = $httpBackend;
-    $httpBackend
-      .when("GET", "https://api.github.com/search/users?key=#=hello")
-      .respond(
-        { items: items }
-      );
-    }));
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend;
+      $httpBackend
+        .expectGET("https://api.github.com/search/users?key=d423d0901c401fb4c4f5d2de4016c4e395741b28&q=hello")
+        .respond(
+          { items: items }
+        );
+      }));
 
-  it('displays search results', function() {
 
-    ctrl.searchTerm = 'hello';
-    ctrl.doSearch();
-    httpBackend.flush();
-    expect(ctrl.searchResult.items).toEqual(items);
+    afterEach(function() {
+     httpBackend.verifyNoOutstandingExpectation();
+     httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('displays search results', function() {
+
+      ctrl.searchTerm = 'hello';
+      ctrl.doSearch();
+      httpBackend.flush();
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
   });
+
 });
